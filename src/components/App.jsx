d6147607d1662164490dfae9b25d328232ef2644
Pratik Shahi <shahipr0@gmail.com>
2021-03-27 17:51:37 +0530

    add function enabled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNote] = useState([]);
+
+  function addNote(note) {
+    setNote((prevValue) => {
+      return [...prevValue, note];
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((element, index) => (
+        <Note key={index} title={element.title} content={element.content} />
+      ))}
+
       <Footer />
     </div>
   );
